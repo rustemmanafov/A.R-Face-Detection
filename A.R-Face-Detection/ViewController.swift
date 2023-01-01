eece197a115b5673acd48345e06e73e02a822c7c
Rustam Manafli <rustemmanafov1@gmail.com>
2023-01-01 17:54:06 +0400

    .

---
+++ A.R-Face-Detection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A.R-Face-Detection
+//
+//  Created by Rustem Manafov on 01.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
