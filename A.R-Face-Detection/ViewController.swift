53df5f82c502c5fb634f925b2a80f1715e59a33a
Rustam Manafli <rustemmanafov1@gmail.com>
2023-01-02 23:05:04 +0400

    updated

--- A.R-Face-Detection/ViewController.swift
+++ A.R-Face-Detection/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var faceView: ARSCNView!
+    @IBOutlet weak var statusLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        faceView.delegate = self
+        
+        guard ARFaceTrackingConfiguration.isSupported else {
+            statusLabel.text = "AR Face Detection is not supported in this device"
+            return
+        }
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = AROrientationTrackingConfiguration()  //ARFaceTrackingConfiguration
+        faceView.session.run(configuration)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        faceView.session.pause()
+    }
+}
 
+extension ViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        guard let device = faceView.device else { return nil }
+        
+        let faceGeometry = ARSCNFaceGeometry(device: device)
+        let faceNote = SCNNode(geometry: faceGeometry)
+        faceNote.geometry?.firstMaterial?.fillMode = .lines
+        return faceNote
+    }
 }
 
